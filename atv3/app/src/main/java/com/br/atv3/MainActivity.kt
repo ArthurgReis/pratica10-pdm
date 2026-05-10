ffc6db765c6ba32c613e230206375df31f4583bd
Arthur Reis <arthur.reis@estudante.iftm.edu.br>
2026-05-10 18:43:55 -0300

    feat: atividade 3

--- atv3/app/src/main/java/com/br/atv3/MainActivity.kt
+++ atv3/app/src/main/java/com/br/atv3/MainActivity.kt
@@ -1,20 +1,100 @@
 package com.br.atv3
 
+import android.content.SharedPreferences
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Switch
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var editUsername: EditText
+    private lateinit var editPassword: EditText
+    private lateinit var switchLembrar: Switch
+    private lateinit var btnEntrar: Button
+    private lateinit var sharedPreferences: SharedPreferences
+
+    private val PREFS_NAME = "PREFERENCIAS"
+    private val CHAVE_USERNAME = "username"
+    private val CHAVE_LEMBRAR = "lembrar"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        editUsername = findViewById(R.id.editUsername)
+
+        editPassword = findViewById(R.id.editPassword)
+
+        switchLembrar = findViewById(R.id.switchLembrar)
+
+        btnEntrar = findViewById(R.id.btnEntrar)
+
+        sharedPreferences = getSharedPreferences(
+            PREFS_NAME,
+            MODE_PRIVATE
+        )
+
+        val lembrar =
+            sharedPreferences.getBoolean(
+                CHAVE_LEMBRAR,
+                false
+            )
+
+        if (lembrar) {
+
+            val usernameSalvo =
+                sharedPreferences.getString(
+                    CHAVE_USERNAME,
+                    ""
+                )
+
+            editUsername.setText(usernameSalvo)
+            switchLembrar.isChecked = true
+        }
+
+        btnEntrar.setOnClickListener {
+
+            val username = editUsername.text.toString()
+
+            val lembrarMarcado = switchLembrar.isChecked
+
+            val editor = sharedPreferences.edit()
+
+            if (lembrarMarcado){
+
+                editor.putString(
+                    CHAVE_USERNAME,
+                    username
+                )
+
+                editor.putBoolean(
+                    CHAVE_LEMBRAR,
+                    true
+                )
+
+            }
+            else {
+
+                editor.remove(CHAVE_USERNAME)
+
+                editor.putBoolean(
+                    CHAVE_LEMBRAR,
+                    false
+
+                )
+            }
+
+            editor.apply()
+
+            Toast.makeText(
+                this,
+                "Login realizado",
+                Toast.LENGTH_SHORT
+            ).show()
         }
     }
 }
